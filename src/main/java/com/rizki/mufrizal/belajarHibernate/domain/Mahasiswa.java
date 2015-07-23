7ff6e4fd3e43105229927d1cca60ec06d88e290e
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-07-23 15:22:43 +0700

    add domain mahasiswa

---
+++ src/main/java/com/rizki/mufrizal/belajarHibernate/domain/Mahasiswa.java
@@ -0,0 +1,72 @@
+package com.rizki.mufrizal.belajarHibernate.domain;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by rizki on 23/07/15.
+ */
+
+@Entity
+@Table(name = "tb_mahasiswa")
+public class Mahasiswa implements Serializable{
+
+    @Id
+    @Column(name = "npm", length = 8)
+    private String npm;
+
+    @Column(name = "nama", nullable = false, length = 45)
+    private String nama;
+
+    @Column(name = "kelas", nullable = false, length = 5)
+    private String kelas;
+
+    @Column(name = "jenisKelamin", nullable = false, length = 6)
+    @Enumerated(EnumType.STRING)
+    private JenisKelamin jenisKelamin;
+
+    @Column(name = "tanggalLahir", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date tanggalLahir;
+
+    public String getNpm() {
+        return npm;
+    }
+
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getKelas() {
+        return kelas;
+    }
+
+    public void setKelas(String kelas) {
+        this.kelas = kelas;
+    }
+
+    public JenisKelamin getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    public void setJenisKelamin(JenisKelamin jenisKelamin) {
+        this.jenisKelamin = jenisKelamin;
+    }
+
+    public Date getTanggalLahir() {
+        return tanggalLahir;
+    }
+
+    public void setTanggalLahir(Date tanggalLahir) {
+        this.tanggalLahir = tanggalLahir;
+    }
+}
